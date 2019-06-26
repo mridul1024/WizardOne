7535b9b95e85dd48ed3bfb578bafc3fe23b04bd8
dhrubanka <dhrubanka10@gmail.com>
2019-06-26 23:09:07 +0530

    dc commit

--- resources/views/layout/baselayout.blade.php
+++ resources/views/layout/baselayout.blade.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
-
-<header>
+<html lang="{{ app()->getLocale() }}">
+<head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="/css/app.css">
-</header>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
 <body>
     @include('inc.navbar')
 
